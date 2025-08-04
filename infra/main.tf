acecbadbfd1d5859b7574277290c6f27b8bc02de
Brayan Urbina <burbinagomez@gmail.com>
2025-08-04 10:18:36 -0500

    Update bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform"
+    bucket         = "terraform-amaris"
     key            = "infra/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
